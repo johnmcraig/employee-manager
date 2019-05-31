980f12b0c74fa81c3bbca459a7a1999ebc0513e7
johnmcraig <jmcraig32884@gmail.com>
2019-05-30 23:43:31 -0400

    Configured new outputs and paths

--- src/EmployeeList/vue.config.js
+++ src/EmployeeList/vue.config.js
@@ -1,6 +1,8 @@
 // vue.config.js
 module.exports = {
-    outputDir: "server/wwwroot/dist/",
+    outputDir: "./server/wwwroot/client",
+    filenameHashing: false,
+    publicPath: "./client/",
     pluginOptions: {
         sourceDir: "client/src/"
     }
