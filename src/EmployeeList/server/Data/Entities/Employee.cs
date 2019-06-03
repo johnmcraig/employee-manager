ee13a432234eaaec119c131c724c7f3ed5b0f7f8
johnmcraig <jmcraig32884@gmail.com>
2019-06-02 22:56:59 -0400

    PLaced Employee class in Data.Entities

--- src/EmployeeList/server/Data/Entities/Employee.cs
+++ src/EmployeeList/server/Data/Entities/Employee.cs
@@ -2,14 +2,16 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace server.Models
+namespace server.Data.Entites
 {
     public class Employee
     {
         [Key]
         public Guid Id { get; set; }
+
         [Required(ErrorMessage = "A name is required for the employee")]
         public string Name { get; set; }
+        
         [Required(ErrorMessage = "A valid email is required")]
         public string Email { get; set; }
     }
