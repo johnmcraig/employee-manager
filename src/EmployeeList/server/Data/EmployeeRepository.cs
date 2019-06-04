0d7ddcbe33fc135b348395b519f51668359de369
johnmcraig <jmcraig32884@gmail.com>
2019-06-03 20:07:30 -0400

    Extended the base repository methods with logging

--- src/EmployeeList/server/Data/EmployeeRepository.cs
+++ src/EmployeeList/server/Data/EmployeeRepository.cs
@@ -1,33 +1,59 @@
+using System;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using server.Data.Entites;
 
 namespace server.Data
 {
     public class EmployeeRepository : IEmployeeRepository
     {
+        private readonly EmployeeDbContext _dbContex;
+        private readonly ILogger<EmployeeRepository> _logger;
+
+        public EmployeeRepository(EmployeeDbContext dbContext, ILogger<EmployeeRepository> logger)
+        {
+            _dbContex = dbContext;
+            _logger = logger;
+        }
+
         public void Add<T>(T entity) where T : class
         {
-            throw new System.NotImplementedException();
+            _logger.LogInformation($"Adding an object of type {entity.GetType()} to the context.");
+
+            _dbContex.Add(entity);
         }
 
         public void Delete<T>(T entity) where T : class
         {
-            throw new System.NotImplementedException();
+            _logger.LogInformation($"Removing an object of type {entity.GetType()} to the context.");
+
+            _dbContex.Remove(entity);
         }
 
-        public Task<Employee> GetEmployee(string name)
+        public async Task<Employee> GetEmployeeAsync(Guid id)
         {
-            throw new System.NotImplementedException();
+            _logger.LogInformation($"Getting a single employee by {id}");
+
+            var employee = await _dbContex.Employees.FirstOrDefaultAsync(x => x.Id == id);
+
+            return employee;
         }
 
-        public Task<Employee[]> GettAllEmployeesAsync()
+        public async Task<Employee[]> GettAllEmployeesAsync()
         {
-            throw new System.NotImplementedException();
+            _logger.LogInformation($"Gettin a all employees");
+
+            var employees = await _dbContex.Employees.ToArrayAsync();
+
+            return employees;
         }
 
-        public Task<bool> SaveChangesAsync()
+        public async Task<bool> SaveChangesAsync()
         {
-            throw new System.NotImplementedException();
+            _logger.LogInformation($"Attempting to save changesd to the context.");
+
+            return (await _dbContex.SaveChangesAsync()) > 0;
         }
     }
 }
