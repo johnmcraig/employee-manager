659db13cebcbde77b6b670703273fef2a028d1f6
johnmcraig <jmcraig32884@gmail.com>
2019-06-15 00:25:02 -0400

    Added bootstrap-vue and bootstrap dependecies

--- src/EmployeeList/client/src/main.js
+++ src/EmployeeList/client/src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap'
+import 'bootstrap-vue/dist/bootstrap-vue'
 import Axios from 'axios'
 
 Vue.config.productionTip = false
 
 Axios.defaults.baseURL = 'https://localhost:5001/api'
+
+Vue.use(BootstrapVue)
 
 new Vue({
   render: h => h(App),
