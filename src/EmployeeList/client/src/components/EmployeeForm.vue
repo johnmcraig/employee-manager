ade496d10c7de8aefca84d974d922e6c31bd4ca7
johnmcraig <jmcraig32884@gmail.com>
2019-06-15 16:37:48 -0400

    Formatted form with bootstrap-vue

--- src/EmployeeList/client/src/components/EmployeeForm.vue
+++ src/EmployeeList/client/src/components/EmployeeForm.vue
@@ -1,12 +1,38 @@
 <template>
     <div id="employee-form">
-        <form @submit.prevent="handleSubmit">
-            <label>Employee name</label>
-            <input v-model="employee.name" type="text" />
-            <label>Employee email</label>
-            <input v-model="employee.email" type="text" />
-            <button>Add Employee</button>
-        </form>
+        <b-form @submit.prevent="onSubmit">
+            <b-form-group
+                id="input-group-1"
+                label="Employee Name:"
+                label-for="input-1">
+                
+                <b-form-input 
+                id="input-1"
+                v-model="employee.name"
+                type="text"
+                required
+                placeholder="Enter name">
+                </b-form-input>
+                
+            </b-form-group>
+
+            <b-form-group
+                id="input-group-2"
+                label="Employee Email:"
+                label-for="input-2">
+                
+                <b-form-input 
+                    id="input-2"
+                    v-model="employee.email" 
+                    type="text"
+                    required
+                    placeholder="Enter a valid email">
+                </b-form-input>
+                
+            </b-form-group>
+
+            <b-button type="submit" variant="primary">Add Employee</b-button>
+        </b-form>
     </div>
 </template>
 
@@ -20,7 +46,8 @@
         }
     },
     methods: {
-        handleSubmit() {
+        onSubmit(evt) {
+            evt.preventDefualt()
             this.$emit('add:employee', this.employee)
         }
     }        
@@ -30,6 +57,7 @@
 <style scoped>
 form {
     margin-bottom: 2rem;
+    padding: 1rem;
+    margin: 1rem;
 }
-
 </style>
