7dcb8cdb26d9b0d57d0317bb407f9f7a492e37f9
johnmcraig <jmcraig32884@gmail.com>
2019-06-15 16:37:21 -0400

    Imported CSS for bootstrap

--- src/EmployeeList/client/src/main.js
+++ src/EmployeeList/client/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue'
 import Axios from 'axios'
 
