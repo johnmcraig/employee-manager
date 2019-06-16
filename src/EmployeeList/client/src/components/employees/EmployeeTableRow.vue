8116f5d658e1a8b7a146caa6d06327701959edfa
johnmcraig <jmcraig32884@gmail.com>
2019-06-16 14:31:23 -0400

    Added seperate row view page for table

---
+++ src/EmployeeList/client/src/components/employees/EmployeeTableRow.vue
@@ -0,0 +1,36 @@
+<template>
+  <tr>
+    <td>{{ employee.name }}</td>
+    <td>{{ employee.email }}</td>
+    <td>
+      <b-button variant="success" @click="onDetailsClick">Details</b-button>
+    </td>
+    <td>
+      <b-button variant="success" @click="onEditClick">Edit</b-button>
+    </td>
+    <td>
+      <b-button variant="danger" @click="onDeleteClick">Delete</b-button>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'employee-table-row',
+  props: [ 'employee' ],
+  methods: {
+    onDetailsClick() {
+        this.$emit('details', this.employee.id)
+    },
+    onEditClick() {
+      this.$emit('edit', this.employee.id)
+    },
+    onDeleteClick() {
+      this.$emit('delete', this.employee.id)
+    },
+  }
+}
+</script>
+
+<style>
+</style>
