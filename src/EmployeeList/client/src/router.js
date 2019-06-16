65e2b718c4a652bed207a71ced7937011b51c630
johnmcraig <jmcraig32884@gmail.com>
2019-06-16 14:31:57 -0400

    Configured not found page and history mode

--- src/EmployeeList/client/src/router.js
+++ src/EmployeeList/client/src/router.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import App from './App'
+import EmployeeDetails from '@/components/employees/EmployeeDetails'
+import NotFound from '@/components/error-pages/NotFound'
 
 Vue.use(Router)
 
 export default new Router({
+    mode: 'history',
     routes: [
-    { path: '/', name: 'employee-list', component: App}
+    { path: '/', name: 'employee-form', component: App },
+    { path: '/employee/:id', name: 'EmployeeDetails', component: EmployeeDetails },
+    { path: '*', name: 'Not Found', component: NotFound }
   ]
 })
