fa65db3a1ded759731400411f15fdbcc0ac63e87
johnmcraig <jmcraig32884@gmail.com>
2019-06-16 23:40:40 -0400

    Created Home page

---
+++ src/EmployeeList/client/src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="small-container">
+        <h1 style="text-align:center;">Employees</h1>
+        <employee-form />
+    </div>
+</template>
+
+<script>
+import EmployeeTable from '@/components/EmployeeTable.vue'
+import EmployeeForm from '@/components/EmployeeForm.vue'
+
+export default {
+    name: 'Home',
+    components: {
+      EmployeeTable,
+      EmployeeForm
+  } 
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
