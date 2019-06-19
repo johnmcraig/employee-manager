5dac4ed72c748eee9a0c7fdcbd8fc9caa3d17ab2
johnmcraig <jmcraig32884@gmail.com>
2019-06-18 21:28:18 -0400

    Added new bootstrap styling and navigation

--- src/EmployeeList/client/src/components/Home.vue
+++ src/EmployeeList/client/src/components/Home.vue
@@ -1,29 +1,37 @@
 <template>
-    <div class="small-container">
-        <h1 style="text-align:center;">Employees</h1>
-        <employee-form />
-        <employee-table />
+    <div class="container">
+        <h2 style="text-align:center;">Welcome to the Employee Entry Form</h2>
+
+        <div class="row">
+            <div class="col col-md-12">
+                <h3>Add New Employees to the Database:</h3>
+                <p>Navigate and fill in the form to add an entry - link here.</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col col-md-12">
+                <h3>See the already existing employees:</h3>
+                <p>Use the button links on the tables to see additional options to edit or delete an entry - link here</p>
+            </div>
+        </div>
+
     </div>
 </template>
 
 <script>
-import EmployeeTable from '@/components/employees/EmployeeTable.vue'
-import EmployeeForm from '@/components/employees/EmployeeForm.vue'
-
 export default {
     name: 'Home',
     components: {
-      EmployeeTable,
-      EmployeeForm
   } 
 }
 </script>
 
 <style scoped>
-.small-container {
+.container {
   width: 100%;
-  max-width: 720px;
-  padding: 1rem;
-  margin: 2rem;
+  max-width: 678px;
+  padding: .5rem;
+  margin: 1rem;
 }
 </style>
