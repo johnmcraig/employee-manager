88a7bcf27c683e6e7b4c90cd93f744e9ea94e488
johnmcraig <jmcraig32884@gmail.com>
2019-06-20 21:48:24 -0400

    Removed commented html

--- src/EmployeeList/client/src/App.vue
+++ src/EmployeeList/client/src/App.vue
@@ -1,10 +1,4 @@
 <template>
-  <!-- <div id="app" class="small-container">
-    <h1 style="text-align:center;">Employees</h1>
-    <employee-form />
-    <employee-table />
-    <router-view />
-  </div> -->
   <b-container id="app">
     <b-row>
       <b-col>
@@ -20,19 +14,12 @@
 </template>
 
 <script>
-// import EmployeeTable from '@/components/EmployeeTable.vue'
-// import EmployeeForm from '@/components/EmployeeForm.vue'
 import Navbar from '@/components/ui/Navbar'
 
 export default {
   name: 'app',
   components: {
     Navbar
-  },
-  data () {
-    return {
-      employees: []
-    }
   }
 }
 </script>
