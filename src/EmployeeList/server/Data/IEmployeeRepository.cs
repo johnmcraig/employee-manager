3ba7368094f72cf973fbc4c683e5e85d5f1c2ad5
johnmcraig <jmcraig32884@gmail.com>
2019-07-17 19:15:31 -0400

    Added update method

--- src/EmployeeList/server/Data/IEmployeeRepository.cs
+++ src/EmployeeList/server/Data/IEmployeeRepository.cs
@@ -8,6 +8,8 @@ namespace server.Data
     {
          void Add<T>(T entity) where T : class;
          void Delete<T>(T entity) where T : class;
+         void Update<T>(T entity) where T : class;
+         
          Task<bool> SaveChangesAsync();
 
          Task<Employee[]> GetAllEmployeesAsync();
