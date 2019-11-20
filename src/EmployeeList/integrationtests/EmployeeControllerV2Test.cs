7d92c04d66a26833bd373334c6363f1f34d79c0d
johnmcraig <jmcraig32884@gmail.com>
2019-11-20 00:04:32 -0500

    Started first controller endpoint test

---
+++ src/EmployeeList/integrationtests/EmployeeControllerV2Test.cs
@@ -0,0 +1,16 @@
+using System.Threading.Tasks;
+
+namespace integrationtests
+{
+    public class EmployeeControllerV2Test : IntergrationTest
+    {
+        public async Task GetAll_RetursEmptyResponse()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
