8b467a6fc42a4203a92891de2443c20395363381
johnmcraig <jmcraig32884@gmail.com>
2019-11-29 00:03:34 -0500

    Added service and options to use inmemory db when testing

--- src/EmployeeList/integrationtests/IntergrationTest.cs
+++ src/EmployeeList/integrationtests/IntergrationTest.cs
@@ -1,6 +1,10 @@
 using System.Net.Http;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using server;
+using server.Data;
 
 namespace integrationtests
 {
@@ -9,7 +13,16 @@ namespace integrationtests
         protected readonly HttpClient _client;
         public IntergrationTest()
         {
-            var appFactory = new WebApplicationFactory<Startup>();
+            var appFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder => {
+                    builder.ConfigureServices(services => {
+                        services.RemoveAll(typeof(EmployeeDbContext));
+                        services.AddDbContext<DbContext>(opt => {
+                            opt.UseInMemoryDatabase("TestDatabase");
+                        });
+                    });
+                });
+
             _client = appFactory.CreateClient();
         }
     }
