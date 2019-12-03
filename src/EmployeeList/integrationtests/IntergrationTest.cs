b913a40560cc150adf6df5769b583646c1040b18
johnmcraig <jmcraig32884@gmail.com>
2019-12-02 23:37:32 -0500

    Changed client to TestClient and added authentication

--- src/EmployeeList/integrationtests/IntergrationTest.cs
+++ src/EmployeeList/integrationtests/IntergrationTest.cs
@@ -1,29 +1,45 @@
 using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using server;
 using server.Data;
+using System.Net.Http.Headers;
+using System;
 
 namespace integrationtests
 {
     public class IntergrationTest
     {
-        protected readonly HttpClient _client;
-        public IntergrationTest()
+        protected readonly HttpClient TestClient;
+        protected IntergrationTest()
         {
             var appFactory = new WebApplicationFactory<Startup>()
-                .WithWebHostBuilder(builder => {
-                    builder.ConfigureServices(services => {
+                .WithWebHostBuilder(builder => 
+                {
+                    builder.ConfigureServices(services => 
+                    {
                         services.RemoveAll(typeof(EmployeeDbContext));
-                        services.AddDbContext<DbContext>(opt => {
+                        services.AddDbContext<EmployeeDbContext>(opt => 
+                        {
                             opt.UseInMemoryDatabase("TestDatabase");
                         });
                     });
                 });
 
-            _client = appFactory.CreateClient();
+            TestClient = appFactory.CreateClient();
+        }
+
+        protected async Task AuthinticateAsync()
+        {
+            TestClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", await GetJwtAsync());
+        }
+
+        protected async Task<string> GetJwtAsync()
+        {
+            throw new NotImplementedException();
         }
     }
 }
