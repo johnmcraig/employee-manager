abba37f5a7816ebb0aee9d609d32921a49cc7441
johnmcraig <jmcraig32884@gmail.com>
2019-12-21 18:13:12 -0500

    Provided services upon startup in Program class

--- src/EmployeeList/server/Program.cs
+++ src/EmployeeList/server/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using server.Data;
 
 namespace server
 {
@@ -14,7 +17,25 @@ namespace server
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var _dbContext = services.GetRequiredService<EmployeeDbContext>();
+                    _dbContext.Database.Migrate();
+                    DataSeeder.SeedData(_dbContext);
+                }
+                catch (Exception ex)
+                {
+                   var logger = services.GetRequiredService<ILogger<Program>>();
+                   logger.LogError(ex, $"An error occurred during migration! Full error here: {ex}");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
