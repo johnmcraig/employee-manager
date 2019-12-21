461db54d983710289586fae4b2a7e210fec9d62f
johnmcraig <jmcraig32884@gmail.com>
2019-12-21 16:26:00 -0500

    Created specific controller test class

---
+++ src/EmployeeList/integrationtests/EmployeeControllerV2Test.cs
@@ -0,0 +1,16 @@
+using System.Threading.Tasks;
+
+namespace integrationtests
+{
+    public class EmployeeControllerV2Test : IntegrationTest
+    {
+        public async Task GetAll_ReturnsEmptyResponse()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
