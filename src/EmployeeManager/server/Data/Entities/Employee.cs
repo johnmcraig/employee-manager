dece85e9b9173f5a2de99863136bdf9a8594327e
johnmcraig <jmcraig32884@gmail.com>
2020-02-06 10:28:41 -0500

    Removed Id property, implemented BaseEntity to classes, mapped full features to Dtos

--- src/EmployeeManager/server/Data/Entities/Employee.cs
+++ src/EmployeeManager/server/Data/Entities/Employee.cs
@@ -4,14 +4,11 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace server.Data.Entities
 {
-    public class Employee
+    public class Employee : BaseEntity
     {
-        [Key]
-        public Guid Id { get; set; }
-
         [Required(ErrorMessage = "A name is required")]
         public string Name { get; set; }
-        
+
         [Required(ErrorMessage = "A valid email is required")]
         public string Email { get; set; }
 
@@ -30,5 +27,14 @@ namespace server.Data.Entities
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime? EndDate { get; set; }
+
+        /* As complexity grows, begin to add features of the entity that can be individually edited 
+        [Required(ErrorMessage = "A name is required")]
+        public string FirstName { get; set; } 
+        [Required(ErrorMessage = "A name is required")]
+        public string LastName { get; set; }  
+        [NotMapped]
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
+        */
     }
 }
--- src/EmployeeManager/server/Dtos/EmployeeDto.cs
+++ src/EmployeeManager/server/Dtos/EmployeeDto.cs
@@ -5,21 +5,26 @@ namespace server.Dtos
 {
     public class EmployeeDto
     {
-        [Key]
-        public Guid Id { get; set; }
-
         [Required(ErrorMessage = "A name is required")]
         public string Name { get; set; }
-        
+
         [Required(ErrorMessage = "A valid email is required")]
         public string Email { get; set; }
 
         public string PhoneNumber { get; set; }
-        
+
         public string Position { get; set; }
-        
-        [DataType(DataType.Date)]  
-        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)] 
-        public DateTime? StartDate { get; set; }  
+
+        public decimal Salary { get; set; }
+
+        public decimal HourlyRate { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? EndDate { get; set; } 
     }
 }
