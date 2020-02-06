fa1e33e0e738b19abeb204e1f6797c89169e18c7
johnmcraig <jmcraig32884@gmail.com>
2020-02-06 09:46:02 -0500

    Commented out class until further testing implementation is done

--- src/EmployeeManager/integrationtests/EmployeeControllerV2Test.cs
+++ src/EmployeeManager/integrationtests/EmployeeControllerV2Test.cs
@@ -5,16 +5,16 @@ using System.Linq;
 
 namespace integrationtests
 {  
-    public class EmployeeControllerV2Test : IntegrationTest
-    {
-        [Fact]
-        public void DeleteEmployee_ShouldRemoveFromDb()
-        {   
-            var storage = new _dbContext;
-            var controller = new EmployeesController(_dbContext.Object);
-            var result = controller.GetAll();
-            Assert.Equal(2, result.Count());
-        }
+    // public class EmployeeControllerV2Test : IntegrationTest
+    // {
+    //     [Fact]
+    //     public void DeleteEmployee_ShouldRemoveFromDb()
+    //     {   
+    //         var storage = new _dbContext;
+    //         var controller = new EmployeesController(_dbContext.Object);
+    //         var result = controller.GetAll();
+    //         Assert.Equal(2, result.Count());
+    //     }
         
-    }
+    // }
 }
