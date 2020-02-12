10213fd5b7bb74424375bca8498e7f7110cfe461
johnmcraig <jmcraig32884@gmail.com>
2020-02-11 19:50:51 -0500

    Added Id propertity

--- src/EmployeeManager/server/Data/Entities/AppUser.cs
+++ src/EmployeeManager/server/Data/Entities/AppUser.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace server.Data.Entities
 {
-    public class AppUser : BaseEntity
+    public class AppUser
     {
+        [Key]
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         [NotMapped]
