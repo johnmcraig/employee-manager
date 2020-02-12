94bb1fba5d4197bb1ee23b9a52e38a99fc9854d8
johnmcraig <jmcraig32884@gmail.com>
2020-02-11 19:48:06 -0500

    Modified AppUser properties

--- src/EmployeeManager/server/Data/Entities/AppUser.cs
+++ src/EmployeeManager/server/Data/Entities/AppUser.cs
@@ -1,10 +1,14 @@
-using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace server.Data.Entities
 {
     public class AppUser : BaseEntity
     {
-        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [NotMapped]
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
+        public string Email { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
     }
