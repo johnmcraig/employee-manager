9d3284e79414f359ab8ea2dac15a146074931759
johnmcraig <jmcraig32884@gmail.com>
2020-02-13 10:31:12 -0500

    Refactored AppUSer to Identity standards

--- src/EmployeeManager/server/Data/Entities/AppUser.cs
+++ src/EmployeeManager/server/Data/Entities/AppUser.cs
@@ -1,18 +1,14 @@
+using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace server.Data.Entities
 {
-    public class AppUser
+    public class AppUser : IdentityUser
     {
-        [Key]
-        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         [NotMapped]
         public string FullName { get { return $"{FirstName} {LastName}"; } }
-        public string Email { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
     }
 }
